cb31f4b365416b256f5472b24471325d488907ff
ariel.pineiro@iskaypet.com <arielsrv@gmail.com>
2023-02-19 22:29:36 +0100

    fix

--- src/resources/docs/docs.go
+++ src/resources/docs/docs.go
@@ -38,7 +38,7 @@ const docTemplate = `{
     }
 }`
 
-// SwaggerInfo holds exported Swagger Info so rest can modify it
+// SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "v1.",
 	Host:             "go-fiber-app.herokuapp.com",
